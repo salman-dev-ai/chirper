f3aa6f5267a0a5801cccf80433cdc38bcd15f68c
salman soft <salman.developer.ai@gmail.com>
2026-07-02 18:14:29 +0300

    add from to create chrip whit tost

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -7,18 +7,34 @@ use App\Models\Chirp;
 
 class ChirpController extends Controller
 {
-    public function index(){
-        // $chirps=[
-        //     ['author'=>'John Doe','message'=>'Hello World!','time'=>'1 minutes ago'],
-        //     ['author'=>'Jane Doe','message'=>'Hello Laravel!','time'=> '1 hour'],
-        //     ['author'=>'Jane Doe','message'=>'Hello Laravel!','time'=> '1 hour'],
-        //     ['author'=>'Jane Doe','message'=>'Hello Laravel!','time'=> '1 hour'],
+    // git data
+    public function index()
+    {
 
-        // ];
-        $chirps= Chirp::with('user')
-        ->latest()
-        ->take(10)->get();
-    
-        return view("home",["chirps"=>$chirps]);
+        $chirps = Chirp::with('user')
+            ->latest()
+            ->take(10)->get();
+
+        return view("home", ["chirps" => $chirps]);
+    }
+
+    public function  store(Request $request)
+    {
+        $validated = $request->validate(
+            [
+                'message' => 'required|string|max:255',
+            ],
+            [
+                'message.required' => 'Please write something to chirp!',
+                'message.max' => 'Chirps must be 255 characters or less.',
+            ]
+        );
+
+        \App\Models\Chirp::create([
+            'message' => $validated['message'],
+            'user_id' => null,
+        ]);
+
+        return redirect('/')->with('success', 'Chirp created!');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChirpController;
 
 Route::get('/', [ChirpController::class, 'index']);
+
+Route::post('/chirps',[ChirpController::class, 'store']);
