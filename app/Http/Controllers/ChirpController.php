e90859c94636eaa4b3bdbcd0a1623a01f995bef3
salman soft <salman.developer.ai@gmail.com>
2026-07-02 22:11:39 +0300

    create Edite and Delete System

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -37,4 +37,32 @@ class ChirpController extends Controller
 
         return redirect('/')->with('success', 'Chirp created!');
     }
+
+    public function edit(Chirp $chirp){
+        return view('chirps.edit',compact('chirp'));
+    }
+
+    public function update(Request $request ,Chirp $chirp){
+        $validated =$request->validate(
+            [
+                'message' => 'required|string|max:255',
+            ],
+            [
+                'message.required' => 'Please write something to chirp!',
+                'message.max' => 'Chirps must be 255 characters or less.',
+            ]
+        );
+
+        // Update the chirp with the validated data
+        $chirp->update($validated);
+
+        return redirect ('/')->with('success', 'Chirp updated!');
+    }
+
+    public function destroy(Chirp $chirp){
+        $chirp->delete();
+
+        return redirect('/')->with('success','Chirp deleted!');
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,10 @@ use App\Http\Controllers\ChirpController;
 Route::get('/', [ChirpController::class, 'index']);
 
 Route::post('/chirps',[ChirpController::class, 'store']);
+
+// Route::get('/chirps/{chirp}/edit',[ChirpController::class,'edit']);
+// // update and delete routes
+// Route::put('/chirps/{chirp}',[ChirpController::class,'update']);
+// Route::delete('/chirps/{chirp}',[ChirpController::class,'destroy']);
+
+Route::resource('chirps', ChirpController::class)->only(['edit', 'update', 'destroy']);
